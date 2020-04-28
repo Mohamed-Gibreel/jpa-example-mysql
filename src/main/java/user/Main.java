6f64f787721261e7ba05b9dd367940586dc5dbe6
Mohamed Gibreel <m.adilgibreel@gmail.com>
2020-04-28 23:40:50 +0200

    Second Commit

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -12,7 +12,35 @@ public class Main {
     private static Faker faker = new Faker();
 
     public static void main(String[] args) {
-      //TODO #1
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("legoset-mysql");
+        EntityManager em = emf.createEntityManager();
+
+        User user = User.builder().username("test").password("123123").build();
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
+        System.out.println(user);
+
+        em.getTransaction().begin();
+        user.setPassword("testPass");
+        em.getTransaction().commit();
+
+        System.out.println(em.find(User.class, "test"));
+        System.out.println(user == em.find(User.class, "test"));
+
+        em.clear();
+
+        System.out.println(em.find(User.class, "test"));
+        System.out.println(user == em.find(User.class, "test"));
+
+        em.getTransaction().begin();
+        User managedUser = em.merge(user);
+        managedUser.setPassword("password");
+        em.getTransaction().commit();
+        System.out.println(em.find(User.class, "test"));
+
+        em.close();
+        emf.close();
     }
 
 }
